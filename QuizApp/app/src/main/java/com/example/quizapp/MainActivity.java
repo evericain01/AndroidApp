93d797f77a3ab87d04ef1cd67f8f953702bbb52f
wornsoulbit <alexandervasil840@gmail.com>
2021-04-06 22:18:41 -0400

    Moved all the API Calls from this file to ApiHandler.

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -6,13 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Open Trivia DB
-    // https://opentdb.com/api.php?amount=10&category=9&difficulty=medium
-    // amount tag is the amount of questions
-    // category tag is only given when a category is selected otherwise its all categories
-    // difficulty is how hard the question is
-    // https://opentdb.com/api_category.php list of all categories in the db
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
