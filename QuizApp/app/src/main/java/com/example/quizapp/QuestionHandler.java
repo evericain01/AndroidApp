047cd1ef57f28db4de7167b5dc90a647dffbec64
wornsoulbit <alexandervasil840@gmail.com>
2021-04-09 02:36:16 -0400

    Added conditions in the constructor making sure that the values given are always in range and if they're not, they're given proper values. Changed the getQuestions method to generateQuestions which deals with the response_code equaling 4 and generates a new set of questions to be given by the method. Also added a way to get difficulty from the information set and finally added safe-guards to the setters the exact same as what was done in the constructor.

--- QuizApp/app/src/main/java/com/example/quizapp/QuestionHandler.java
+++ QuizApp/app/src/main/java/com/example/quizapp/QuestionHandler.java
@@ -9,98 +9,75 @@ public class QuestionHandler extends ApiHandler {
 
     private JSONArray originalJsonArr = null;
     private int amount;
+    private int category;
+    private String difficulty;
+    private String type;
+
     private ArrayList<String> questions;
     private ArrayList<String> answers;
+    private ArrayList<String> difficultyArr;
     private ArrayList<JSONArray> incorrectAnswers;
 
-    /**
-     *
-     * @param amount number of questions with a max of 50
-     */
-    public QuestionHandler(int amount) {
-        if (amount >= 50) {
-            amount = 50;
-        }
-
-        this.amount = amount;
-        originalJsonArr = generateQuestionsArray(amount, -1, "", "");
-
-        try {
-            questions = getQuestions(originalJsonArr);
-            answers = getCorrectAnswers(originalJsonArr);
-            incorrectAnswers = getIncorrectAnswers(originalJsonArr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
-     *
-     * @param amount number of questions with a max of 50
-     * @param category category numbers from 9-27
-     */
-    public QuestionHandler(int amount, int category) {
-        if (amount >= 50) {
-            amount = 50;
-        }
-
-        this.amount = amount;
-        originalJsonArr = generateQuestionsArray(amount, category, "", "");
-
-        try {
-            questions = getQuestions(originalJsonArr);
-            answers = getCorrectAnswers(originalJsonArr);
-            incorrectAnswers = getIncorrectAnswers(originalJsonArr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     *
-     * @param amount number of questions with a max of 50
-     * @param category category numbers from 9-27
+     * @param amount     number of questions with a max of 50
+     * @param category   category numbers from 9-27
      * @param difficulty easy, medium, hard
-     */
-    public QuestionHandler(int amount, int category, String difficulty) {
-        if (amount >= 50) {
-            amount = 50;
-        }
-
-        this.amount = amount;
-        originalJsonArr = generateQuestionsArray(amount, category, difficulty, "");
-
-        try {
-            questions = getQuestions(originalJsonArr);
-            answers = getCorrectAnswers(originalJsonArr);
-            incorrectAnswers = getIncorrectAnswers(originalJsonArr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     *
-     * @param amount number of questions with a max of 50
-     * @param category category numbers from 9-27
-     * @param difficulty easy, medium, hard
-     * @param type boolean or multiple
+     * @param type       boolean or multiple
      */
     public QuestionHandler(int amount, int category, String difficulty, String type) {
         if (amount >= 50) {
             amount = 50;
+        } else {
+            this.amount = amount;
         }
 
-        this.amount = amount;
+        if (category < 9 || category > 32) {
+            this.category = -1;
+        } else {
+            this.category = category;
+        }
+
+        if (difficulty.equals("easy") && !difficulty.equals("medium") && !difficulty.equals("hard")) {
+            this.difficulty = "easy";
+        } else if (!difficulty.equals("easy") && difficulty.equals("medium") && !difficulty.equals("hard")){
+            this.difficulty = "medium";
+        } else if (!difficulty.equals("easy") && !difficulty.equals("medium") && difficulty.equals("hard")) {
+            this.difficulty = "hard";
+        } else {
+            this.difficulty = "";
+        }
+
+        if (type.equals("boolean") && !type.equals("multiple")) {
+            this.type = "boolean";
+        } else if (!type.equals("boolean") && type.equals("multiple")){
+            this.type = "multiple";
+        } else {
+            this.type = "";
+        }
+
+        generateQuestions();
+    }
+
+    protected boolean generateQuestions() {
+        boolean flag = true;
         originalJsonArr = generateQuestionsArray(amount, category, difficulty, type);
 
+        if (getResponseCode().equals("4")) {
+            originalJsonArr = generateQuestionsArray(amount, category, difficulty, type);
+            flag = false;
+        }
+
         try {
-            questions = getQuestions(originalJsonArr);
+            questions = getQuestionsArray(originalJsonArr);
             answers = getCorrectAnswers(originalJsonArr);
             incorrectAnswers = getIncorrectAnswers(originalJsonArr);
+            difficultyArr = getDifficultyArray(originalJsonArr);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        return flag;
     }
 
     /**
@@ -110,7 +87,7 @@ public class QuestionHandler extends ApiHandler {
      * @return ArrayList<String> of all the questions.
      * @throws JSONException If JSON fails to parse
      */
-    protected ArrayList<String> getQuestions(JSONArray jsonArray) throws JSONException {
+    private ArrayList<String> getQuestionsArray(JSONArray jsonArray) throws JSONException {
         ArrayList<String> questionArr = new ArrayList<>();
 
         for (int i = 0; i < jsonArray.length(); i++) {
@@ -120,6 +97,16 @@ public class QuestionHandler extends ApiHandler {
         return questionArr;
     }
 
+    private ArrayList<String> getDifficultyArray(JSONArray jsonArray) throws JSONException {
+        ArrayList<String> difficultyArr = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            difficultyArr.add(jsonArray.getJSONObject(i).getString("difficulty"));
+        }
+
+        return difficultyArr;
+    }
+
     /**
      * Gets a array of incorrect answers from a given JSONArray.
      *
@@ -127,7 +114,7 @@ public class QuestionHandler extends ApiHandler {
      * @return ArrayList<String> of all the questions.
      * @throws JSONException If JSON fails to parse
      */
-    protected ArrayList<JSONArray> getIncorrectAnswers(JSONArray jsonArray) throws JSONException {
+    private ArrayList<JSONArray> getIncorrectAnswers(JSONArray jsonArray) throws JSONException {
         ArrayList<JSONArray> incorrectArr = new ArrayList<>();
 
         for (int i = 0; i < jsonArray.length(); i++) {
@@ -144,7 +131,7 @@ public class QuestionHandler extends ApiHandler {
      * @return ArrayList<String> of all the questions.
      * @throws JSONException If JSON fails to parse
      */
-    protected ArrayList<String> getCorrectAnswers(JSONArray jsonArray) throws JSONException {
+    private ArrayList<String> getCorrectAnswers(JSONArray jsonArray) throws JSONException {
         ArrayList<String> correctArr = new ArrayList<>();
 
         for (int i = 0; i < jsonArray.length(); i++) {
@@ -154,9 +141,7 @@ public class QuestionHandler extends ApiHandler {
         return correctArr;
     }
 
-    public ArrayList<String> getQuestions() {
-        return questions;
-    }
+    public ArrayList<String> getQuestions() { return questions; }
 
     public ArrayList<String> getAnswers() {
         return answers;
@@ -166,7 +151,55 @@ public class QuestionHandler extends ApiHandler {
         return incorrectAnswers;
     }
 
+    public ArrayList<String> getDifficultyArr() {
+        return difficultyArr;
+    }
+
     public int getAmount() {
         return amount;
+    }
+
+    public void setAmount(int amount) {
+        if (amount >= 50) {
+            amount = 50;
+        } else {
+            this.amount = amount;
+        }
+    }
+
+    public int getCategory() { return category; }
+
+    public void setCategory(int category) {
+        if (category < 9 || category > 32) {
+            this.category = -1;
+        } else {
+            this.category = category;
+        }
+    }
+
+    public String getDifficulty() { return difficulty; }
+
+    public void setDifficulty(String difficulty) {
+        if (difficulty.equals("easy") && !difficulty.equals("medium") && !difficulty.equals("hard")) {
+            this.difficulty = "easy";
+        } else if (!difficulty.equals("easy") && difficulty.equals("medium") && !difficulty.equals("hard")){
+            this.difficulty = "medium";
+        } else if (!difficulty.equals("easy") && !difficulty.equals("medium") && difficulty.equals("hard")) {
+            this.difficulty = "hard";
+        } else {
+            this.difficulty = "";
+        }
+    }
+
+    public String getType() { return type; }
+
+    public void setType(String type) {
+        if (type.equals("boolean") && !type.equals("multiple")) {
+            this.type = "boolean";
+        } else if (!type.equals("boolean") && type.equals("multiple")){
+            this.type = "multiple";
+        } else {
+            this.type = "";
+        }
     }
 }
