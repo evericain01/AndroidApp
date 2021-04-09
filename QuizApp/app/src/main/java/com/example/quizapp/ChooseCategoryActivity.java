569fe13d8068f885bf970df9590052c70ed2a65e
Christian Paul david <christianpauld002@gmai.com>
2021-04-09 19:18:50 -0400

    added category drop down

--- QuizApp/app/src/main/java/com/example/quizapp/ChooseCategoryActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/ChooseCategoryActivity.java
@@ -3,12 +3,33 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class ChooseCategoryActivity extends AppCompatActivity {
+public class ChooseCategoryActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Integer category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_category);
+
+        Spinner category = findViewById(R.id.chooseCategory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category.setAdapter(adapter);
+        category.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        category = i + 9;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
