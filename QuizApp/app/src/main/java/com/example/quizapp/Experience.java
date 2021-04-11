681432f88b1817df1e19f24773e74667e2ed56b0
wornsoulbit <alexandervasil840@gmail.com>
2021-04-10 22:43:35 -0400

    Created a class to calculate the experience, the progression bar rates and how much experience is needed for the next level.

---
+++ QuizApp/app/src/main/java/com/example/quizapp/Experience.java
@@ -0,0 +1,110 @@
+package com.example.quizapp;
+
+import java.util.ArrayList;
+
+public class Experience {
+
+    private final double LEVEL_MULTIPLIER = 0.1;
+    private final double EASY_MULTIPLIER = 1;
+    private final double MED_MULTIPLIER = 1.5;
+    private final double HARD_MULTIPLIER = 3;
+    private final int CORRECT_QUESTION_CONST = 30;
+
+    /**
+     * Calculates the experience based off of the difficulty given to the function.
+     *
+     * @param difficulty How hard a question was, easy, medium or hard.
+     * @return Total experience a user gained.
+     */
+    public double calculateExperience(ArrayList<String> difficulty) {
+        double totalExperienceGained = 0;
+
+        for (int i = 0; i < difficulty.size() - 1; i++) {
+            switch (difficulty.get(i)) {
+                case "easy":
+                    totalExperienceGained += EASY_MULTIPLIER * CORRECT_QUESTION_CONST;
+                    break;
+                case "medium":
+                    totalExperienceGained += MED_MULTIPLIER * CORRECT_QUESTION_CONST;
+                    break;
+                case "hard":
+                    totalExperienceGained += HARD_MULTIPLIER * CORRECT_QUESTION_CONST;
+                    break;
+            }
+        }
+        return totalExperienceGained;
+    }
+
+    /**
+     * Gets how far along a user is in a current level, based off of the difference of the level
+     * experience difference and the xp difference divided by the level xp difference.
+     * Subtracting by one to reverse the number then multiplying by -100 to get a number between
+     * 0 and 100.
+     *
+     * @param currentExperience The current experience that a user has.
+     * @return An integer between 0 and 100 based off of how far into a level a user is.
+     */
+    public int progressionRate(double currentExperience) {
+        // Get the current level based on the current player experience.
+        int currLevel =  calculateLevel(currentExperience);
+
+        // Calculate the experience of the current level and the next level.
+        double currLevelXp = calculateLevelExperience(currLevel);
+        double nextLevelXp = calculateLevelExperience(currLevel + 1);
+
+        // Difference between the next level total experience and the current level total experience.
+        double levelXpDiff = nextLevelXp - currLevelXp;
+        // Difference between the current experience and the difference of experience of the two levels.
+        double xpDiff = currentExperience - levelXpDiff;
+
+        // Gets how far along a user is in a current level, based off of the difference of the level
+        // experience difference and the xp difference divided by the level xp difference.
+        // Subtracting by one to reverse the number then multiplying by -100 to get a number between
+        // 0 and 100.
+        return (int) (-100 * ((((levelXpDiff - xpDiff) / levelXpDiff) - 1)));
+    }
+
+    /**
+     * Gets the experience needed to the next level.
+     *
+     * @param currentExperience The current experience that a user has.
+     * @return The experience a user needs to obtain the next level.
+     */
+    public double nextLevelXpNeeded(double currentExperience) {
+        // Get the current level based on the current player experience.
+        int currLevel =  calculateLevel(currentExperience);
+
+        // Calculate the experience of the current level and the next level.
+        double currLevelXp = calculateLevelExperience(currLevel);
+        double nextLevelXp = calculateLevelExperience(currLevel + 1);
+
+        // Difference between the next level total experience and the current level total experience.
+        double levelXpDiff = nextLevelXp - currLevelXp;
+
+        // Difference between the current experience and the difference of experience of the two levels.
+        return currentExperience - levelXpDiff;
+    }
+
+    /**
+     * Calculates the experience a given level needs.
+     *
+     * @param level Level's experience to be calculated.
+     * @return The experience needed to obtain a specified level.
+     */
+    private double calculateLevelExperience(int level) {
+        return (Math.pow(level, 2) / LEVEL_MULTIPLIER);
+    }
+
+    /**
+     * Calculates the current level based on the current experience a user has.
+     *
+     * @param currentExperience The current experience that a user has.
+     * @return The level the user is at.
+     */
+    private int calculateLevel(double currentExperience) {
+        int currLevel;
+
+        return (int)(LEVEL_MULTIPLIER * Math.sqrt(currentExperience));
+    }
+
+}
